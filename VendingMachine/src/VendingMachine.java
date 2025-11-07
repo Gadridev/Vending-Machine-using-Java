d51d5ef53466c885b1e03dbd0df4a4ad0d2f7c6e
mohamed gadri <gadrimohamed8@gmail.com>
2025-11-07 12:49:11 +0100

    Initial project setup

---
+++ VendingMachine/src/VendingMachine.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class VendingMachine {
+    ArrayList<String> produits = new ArrayList<>(List.of("Eau", "Soda", "Chips", "Chocolat"));
+    ArrayList<Double> prix = new ArrayList<>(List.of(5.0, 8.0, 12.0, 15.0));
+    ArrayList<Integer> stock = new ArrayList<>(List.of(10, 5, 7, 3));
+
+    public void menuDisplat(){
+
+    }
+    public void purchaseLogic(){
+
+    }
+    public void updateStock(){
+
+    }
+    public void improvement(){
+
+    }
+}
