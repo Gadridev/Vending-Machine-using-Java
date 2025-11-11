4305ed6e5339e444d9f3de1a1814d873c68908a1
Gadri Mohamed <gadrimohamed8@gmail.com>
2025-11-11 11:36:22 +0100

    Restore my missing code

--- VendingMachine/src/VendingMachine.java
+++ VendingMachine/src/VendingMachine.java
@@ -3,8 +3,10 @@ import java.util.List;
 import java.util.Scanner;
 
 public class VendingMachine {
-
-
+   Scanner scanner=new Scanner(System.in);
+    ArrayList<String> produits = new ArrayList<>(List.of("Eau", "Soda", "Chips", "Chocolat"));
+    ArrayList<Double> prix = new ArrayList<>(List.of(5.0, 8.0, 12.0, 15.0));
+    ArrayList<Integer> stock = new ArrayList<>(List.of(10, 5, 7, 3));
 
 
 
