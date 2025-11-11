f086e31bef427eb3a32ec553c5ddaf1bf4264ec2
Gadri Mohamed <93617218+Gadridev@users.noreply.github.com>
2025-11-11 12:09:04 +0100

    Remove updateStock method from VendingMachine
    Removed the updateStock method from VendingMachine class.

--- VendingMachine/src/VendingMachine.java
+++ VendingMachine/src/VendingMachine.java
@@ -79,21 +79,13 @@ public class VendingMachine {
 
 
 
-    public void updateStock(){
-            System.out.println("--- Mode Administrateur: Recharger le stock ---");
-            for (int i = 0; i < produits.size(); i++) {
-                System.out.println(produits.get(i) + " - Stock actuel : " + stock.get(i));
-                System.out.print("Quantité à ajouter : ");
-                int ajout;
-                if (scanner.hasNextInt()) {
-                    ajout = scanner.nextInt();
-                } else {
-                    System.out.println("Entrée invalide !");
-                    scanner.next();
-                    ajout = 0;
-                }
-                stock.set(i, stock.get(i) + ajout);
-            }
-            System.out.println("Stock mis à jour avec succès !");
-    }
+   
+
+
+
+
+
+
+
+
 }
