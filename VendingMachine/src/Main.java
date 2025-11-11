91757cd8e38213afd60b2c035ba3bfbae8ee9c29
mohamed gadri <gadrimohamed8@gmail.com>
2025-11-11 07:13:15 +0100

    updateStock is finished

--- VendingMachine/src/Main.java
+++ VendingMachine/src/Main.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
+
 public class Main {
-    public void main(){
+    public static void main(){
         System.out.print("just for testing");
+        VendingMachine v=new VendingMachine();
+        Scanner s=new Scanner(System.in);
+        boolean quitter=false;
+        while (!quitter) {
+            v.afficherMenu();
+            int choix=s.nextInt();
+            switch (choix) {
+                case 1:
+                    v.afficherProduits();
+                    break;
+                case 2:
+                    v.acheterProduit();
+                    break;
+                case 3:
+                    System.out.println("Merci d'avoir utilisé le distributeur !");
+                    quitter = true;
+                    break;
+                case 4:
+                    v.updateStock();
+                    break;
+                default:
+                    System.out.println("Choix invalide, réessayez.");
+            }
+            System.out.println();
+        }
     }
 }
--- VendingMachine/src/VendingMachine.java
+++ VendingMachine/src/VendingMachine.java
@@ -1,21 +1,83 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class VendingMachine {
-    ArrayList<String> produits = new ArrayList<>(List.of("Eau", "Soda", "Chips", "Chocolat"));
-    ArrayList<Double> prix = new ArrayList<>(List.of(5.0, 8.0, 12.0, 15.0));
-    ArrayList<Integer> stock = new ArrayList<>(List.of(10, 5, 7, 3));
 
-    public void menuDisplat(){
 
-    }
-    public void purchaseLogic(){
 
-    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     public void updateStock(){
-
-    }
-    public void improvement(){
-
+            System.out.println("--- Mode Administrateur: Recharger le stock ---");
+            for (int i = 0; i < produits.size(); i++) {
+                System.out.println(produits.get(i) + " - Stock actuel : " + stock.get(i));
+                System.out.print("Quantité à ajouter : ");
+                int ajout;
+                if (scanner.hasNextInt()) {
+                    ajout = scanner.nextInt();
+                } else {
+                    System.out.println("Entrée invalide !");
+                    scanner.next();
+                    ajout = 0;
+                }
+                stock.set(i, stock.get(i) + ajout);
+            }
+            System.out.println("Stock mis à jour avec succès !");
     }
 }
