fe3a0ebdf951ea352c1f4890f4f18df6a80589da
Amal <amalbasbas55@gmail.com>
2025-11-11 12:01:57 +0100

    Meny display is finished

--- VendingMachine/src/Main.java
+++ VendingMachine/src/Main.java
@@ -1,32 +1,48 @@
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Main {
-    public static void main(){
-        System.out.print("just for testing");
-        VendingMachine v=new VendingMachine();
-        Scanner s=new Scanner(System.in);
-        boolean quitter=false;
-        while (!quitter) {
-            v.afficherMenu();
-            int choix=s.nextInt();
-            switch (choix) {
-                case 1:
-                    v.afficherProduits();
-                    break;
-                case 2:
-                    v.acheterProduit();
-                    break;
-                case 3:
-                    System.out.println("Merci d'avoir utilisé le distributeur !");
-                    quitter = true;
-                    break;
-                case 4:
-                    v.updateStock();
-                    break;
-                default:
-                    System.out.println("Choix invalide, réessayez.");
+
+        static  ArrayList<String> produits = new ArrayList<> (List.of("Eau", "Soda", "Chips", "Chocolat"));
+        static ArrayList<Double> prix = new ArrayList<>(List.of(5.0, 8.0, 12.0, 15.0));
+        static ArrayList<Integer> stock = new ArrayList<>(List.of(10, 5, 7, 3));
+
+
+        public static void main(String[] args) {
+
+            Scanner scanner = new Scanner(System.in);
+
+            int choix = 0;
+            while (choix != 3) {
+                afficherMenu();
+                System.out.println("Entrez votre choix: ");
+                choix = scanner.nextInt();
+                if (choix == 1) {
+                    afficherProduits();
+                } else if (choix == 2) {
+                    //acheterProduits();
+                } else if (choix == 3) {
+                    System.out.println("Au revoir!");
+                } else {
+                    System.out.println("choix invalide, veuillez réessayer ");
+                }
+
             }
-            System.out.println();
+        }
+
+        public static void afficherMenu() {
+            System.out.println("=== Distributeur Automatique ===");
+            System.out.println("1. Afficher les produits");
+            System.out.println("2. Acheter un produit");
+            System.out.println("3. Quitter");
+
+        }
+
+        public static void afficherProduits() {
+            System.out.println("les produits disponibles: ");
+            for (int i = 0; i < produits.size(); i++) {
+                System.out.println(i + 1 + "." + produits.get(i) + " ( " + prix.get(i) + " MAD )" + "- Stock: " + stock.get(i));
+            }
         }
     }
-}
